ba456fd3b471f146f9e2cea0c37c995c3ba22c66
CaolanCode <cmacmahon3@gmail.com>
2022-12-15 20:12:49 +0000

    fixed updateDisplay to show previous number after compute

--- app.js
+++ app.js
@@ -1,63 +1,62 @@
 class Calculator{
-    constructor(displayContainer){
-        this.displayContainer = displayContainer;
-        this.clear();
+  constructor(displayContainer){
+    this.displayContainer = displayContainer;
+    this.clear();
+  }
+  appendNumber(number){
+    this.currentNumber = this.currentNumber.toString() + number.toString();
+  }
+  updateDisplay(){
+    if(this.operator !== undefined){
+      this.displayContainer.innerText = `${this.previousNumber} ${this.operator} ${this.currentNumber}`;
+    } else if(this.currentNumber === '' && this.operator === undefined){
+      this.displayContainer.innerText = this.previousNumber;
+    } else {
+      this.displayContainer.innerText = this.currentNumber;
     }
-    appendNumber(number){
-       this.currentNumber = this.currentNumber.toString() + number.toString();
+  }
+  assignOperator(operator){
+    if(this.operator !== undefined){
+      this.compute();
+    } else{
+      this.previousNumber = this.currentNumber;
+      this.currentNumber = '';
     }
-    updateDisplay(){
-        if(this.previousNumber !== ''){
-            this.displayContainer.innerText = `${this.previousNumber} ${this.operator} ${this.currentNumber}` ;
-        } else if(this.operator !== undefined){
-            this.displayContainer.innerText = `${this.currentNumber} ${this.operator}`;
-        } else {
-            this.displayContainer.innerText = this.currentNumber;
-        }
+    this.operator = operator
+  }
+  compute(){
+    let result;
+    let prev = parseFloat(this.previousNumber);
+    let current = parseFloat(this.currentNumber);
+    if(isNaN(prev) || isNaN(current)) return;
+    switch(this.operator){
+      case '+':
+        result = prev + current;
+        break;
+      case '−':
+        result = prev - current;
+        break;
+      case '×':
+        result = prev * current;
+        break;
+      case '÷':
+        result = prev / current;
+        break;
+      default:
+        return;
     }
-    assignOperator(operator){
-        if(this.operator !== undefined){
-            this.compute();
-        } else{
-            this.previousNumber = this.currentNumber;
-            this.currentNumber = '';
-        }
-        this.operator = operator
-        console.log("operator: " + this.operator);
-    }
-    compute(){
-        let result;
-        let prev = parseFloat(this.previousNumber);
-        let current = parseFloat(this.currentNumber);
-        if(isNaN(prev) || isNaN(current)) return;
-        switch(this.operator){
-            case '+':
-                result = prev + current;
-                break;
-            case '−':
-                result = prev - current;
-                break;
-            case '×':
-                result = prev * current;
-                break;
-            case '÷':
-                result = prev / current;
-                break;
-            default:
-                return;
-        }
-        this.currentNumber = result;
-        this.operator = undefined;
-        this.previousNumber = '';
-    }
-    delete(){
-        this.currentNumber = this.currentNumber.toString().slice(0, -1);
-    }
-    clear(){
-        this.currentNumber = '';
-        this.previousNumber = '';
-        this.operator = undefined;
-    }
+    this.currentNumber = '';
+    this.operator = undefined;
+    this.previousNumber = result;
+  }
+  delete(){
+    this.currentNumber = this.currentNumber.toString().slice(0, -1);
+  }
+  clear(){
+    this.currentNumber = '';
+    this.previousNumber = '';
+    this.operator = undefined;
+  }
 }
 const displayContainer = document.getElementById('display');
 const numberButton = document.querySelectorAll('.number');
@@ -69,30 +68,30 @@ const equals = document.getElementById('equals');
 const calculator = new Calculator(displayContainer);
 
 numberButton.forEach(button => {
-    button.addEventListener('click', () => {
-        calculator.appendNumber(button.innerText);
-        calculator.updateDisplay();
-    })
+  button.addEventListener('click', () => {
+    calculator.appendNumber(button.innerText);
+    calculator.updateDisplay();
+  })
 })
 
 operatorButton.forEach(button => {
-    button.addEventListener('click', () => {
-        calculator.assignOperator(button.innerText);
-        calculator.updateDisplay();
-    })
+  button.addEventListener('click', () => {
+    calculator.assignOperator(button.innerText);
+    calculator.updateDisplay();
+  })
 })
 
 deleteButton.addEventListener('click', () => {
-    calculator.delete();
-    calculator.updateDisplay();
+  calculator.delete();
+  calculator.updateDisplay();
 })
 
 allClearButton.addEventListener('click', () => {
-    calculator.clear();
-    calculator.updateDisplay();
+  calculator.clear();
+  calculator.updateDisplay();
 })
 
 equals.addEventListener('click', () => {
-    calculator.compute();
-    calculator.updateDisplay();
+  calculator.compute();
+  calculator.updateDisplay();
 })
