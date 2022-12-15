5a371cd0532d45638d163b7999175d0c665efcd1
CaolanCode <cmacmahon3@gmail.com>
2022-12-15 14:07:29 +0000

    append numbers together

--- app.js
+++ app.js
@@ -1,8 +1,24 @@
+class Calculator{
+    constructor(display){
+        this.display = display;
+        this.displayText = '';
+    }
+
+    appendNumber(number){
+        this.displayText = this.displayText.toString() + number.toString();
+    }
+    updateDisplay(){
+        this.display.innerText = this.displayText;
+    }
+}
 const numberButton = document.querySelectorAll('.number');
 const display = document.querySelector('.display');
 
+const calculator = new Calculator(display)
+
 numberButton.forEach(button => {
     button.addEventListener('click', () => {
-        display.innerText = button.innerText;
+        calculator.appendNumber(button.innerText);
+        calculator.updateDisplay();
     })
 })
